974e58ea96c7a82a8dbbbcf8a54ccb4ab6508a19
gazibillalhossain-lgtm <gazibillalhossain@gmail.com>
2026-08-17 18:59:11 +0600

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/screenlockapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.screenlockapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+
+        btnStart.setOnClickListener {
+            if (checkOverlayPermission()) {
+                val intent = Intent(this, OverlayService::class.java)
+                startService(intent)
+            } else {
+                requestOverlayPermission()
+            }
+        }
+    }
+
+    private fun checkOverlayPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Settings.canDrawOverOtherApps(this)
+        } else true
+    }
+
+    private fun requestOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, 1234)
+        }
+    }
+}
